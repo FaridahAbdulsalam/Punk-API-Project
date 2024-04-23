07a4ae9a9309675f086a57afad50cf4f086de5d9
Faridah Salami <f.salami@outlook.com>
2024-04-23 16:19:04 +0100

    Filter checkboxes styled: inner box still visible, needs fix

--- src/Components/FilterList/FilterList.tsx
+++ src/Components/FilterList/FilterList.tsx
@@ -1,35 +1,35 @@
 import { ChangeEventHandler } from "react";
+import "./FilterList.scss";
 
-const filters = 
-[
-    {id: 1, value: "abv", label: "High ABV > 6.0%"},
-    {id: 2, value: "first_brewed", label: "Classic Range"},
-    {id: 3, value: "ph", label: "Acidic (ph < 4)"},
+const filters = [
+  { id: 1, value: "abv", label: "High ABV > 6.0%" },
+  { id: 2, value: "first_brewed", label: "Classic Range" },
+  { id: 3, value: "ph", label: "Acidic (ph < 4)" },
 ];
 
 type FilterListProps = {
-    handleFilter: ChangeEventHandler<HTMLInputElement>;
-    checked: string;
-}
+  handleFilter: ChangeEventHandler<HTMLInputElement>;
+  checked: string;
+};
 
-const FilterList = ({handleFilter, checked}: FilterListProps) => {
+const FilterList = ({ handleFilter, checked }: FilterListProps) => {
   return (
     <div className="filter">
-       {filters.map((filter => (
-        <label key={filter.id}>
-            <input
+      {filters.map((filter) => (
+        <label className="filter__label" key={filter.id}>
+          <input
+            className="filter__checkbox"
             type="checkbox"
             value={filter.value}
             checked={checked === filter.value}
             onChange={handleFilter}
             />
+            <span className="filter__checkmark"></span>
             {filter.label}
         </label>
-       )))}
+      ))}
     </div>
   );
 };
 
 export default FilterList;
-
-
