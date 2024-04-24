6625818a81eba30c879cf363b3db4cb18889852a
Faridah Salami <f.salami@outlook.com>
2024-04-24 23:09:35 +0100

    filters and if statements reapplied to checkboxes

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ import { Beer } from "./types/types";
 const App = () => {
   const [beerData, setBeerData] = useState<Beer[]>([]);
   const [searchTerm, setSearchTeam] = useState<string>("");
-  const [isCheckedABV, setIsCheckedABV] = useState<boolean>(false)
-  const [isCheckedFirstBrewed, setIsCheckedFirstBrewed] = useState<boolean>(false)
-  const [isCheckedPH, setIsCheckedPH] = useState<boolean>(false)
+  const [isCheckedABV, setIsCheckedABV] = useState<boolean>(false);
+  const [isCheckedFirstBrewed, setIsCheckedFirstBrewed] =
+    useState<boolean>(false);
+  const [isCheckedPH, setIsCheckedPH] = useState<boolean>(false);
 
   // const [selectedFilter, setSelectedFilter] = useState("");
-  
-  
+
   // const getData = async () => {
   //   const url = "http://localhost:3333/v2/beers?page=1&per_page=80";
   //   const response = await fetch(url);
@@ -30,42 +30,56 @@ const App = () => {
   };
 
   const handleFilter = (event: ChangeEvent<HTMLInputElement>) => {
-    const selectedFilter = event.currentTarget.value
+    const selectedFilter = event.currentTarget.value;
     const filterInput = event.currentTarget.checked;
     console.log(selectedFilter);
-    if(selectedFilter === "abv"){
-      setIsCheckedABV(filterInput)
-    }else if(selectedFilter === "first_brewed"){
-      setIsCheckedFirstBrewed(filterInput)
-    }else if(selectedFilter === "ph"){
-      setIsCheckedPH(filterInput)
+    if (selectedFilter === "abv") {
+      setIsCheckedABV(filterInput);
+    } else if (selectedFilter === "first_brewed") {
+      setIsCheckedFirstBrewed(filterInput);
+    } else if (selectedFilter === "ph") {
+      setIsCheckedPH(filterInput);
     }
   };
 
-
   const filteredBeers = beers
-  .filter((beer) => {
-    const date = beer.first_brewed.split("/")
+    .filter((beer) => {
+      const date = beer.first_brewed.split("/");
       const year = Number(date[1]);
+      const all = beer.abv >= 6 && beer.ph <=4 && year >=2010;
 
-    if(isCheckedABV){
-      return beer.abv <=6;
-    } 
-    
-    if (isCheckedPH) {
-      return beer.ph >= 4;
-    }
+      if(isCheckedABV && isCheckedPH && isCheckedFirstBrewed){
+        return all;
+      }
+      
+      if(isCheckedABV && isCheckedPH){
+        return beer.abv >=6 && beer.ph <=4;
+      }
 
-    if (isCheckedFirstBrewed) {
-      return year >= 2010
-    }
+      if(isCheckedABV && isCheckedFirstBrewed){
+        return beer.abv >=6 && year >= 2010;
+      }
 
-    return true;
-  })
-  .filter((beer) => beer.name.toLowerCase().includes(searchTerm)); 
-  
- 
+      if(isCheckedPH && isCheckedFirstBrewed){
+        return beer.ph <= 4 && year >=2010;
+      }
 
+      if (isCheckedABV) {
+        return beer.abv >= 6;
+      }
+
+
+      if (isCheckedPH) {
+        return beer.ph <= 4;
+      }
+
+      if (isCheckedFirstBrewed) {
+        return year >= 2010;
+      }
+
+      return true;
+    })
+    .filter((beer) => beer.name.toLowerCase().includes(searchTerm));
 
   return (
     <div className="display">
@@ -74,8 +88,10 @@ const App = () => {
         label="search"
         handleInput={handleInput}
         onChange={handleFilter}
-        isCheckedABV={isCheckedABV} isCheckedFirstBrewed={isCheckedFirstBrewed} isCheckedPH={isCheckedPH}      
-        />
+        isCheckedABV={isCheckedABV}
+        isCheckedFirstBrewed={isCheckedFirstBrewed}
+        isCheckedPH={isCheckedPH}
+      />
       <CardList beers={filteredBeers} />
     </div>
   );
@@ -83,28 +99,3 @@ const App = () => {
 
 export default App;
 
-
-/*
-
- const filteredBeers = beers
-    .filter((beer) => {
-      const date = beer.first_brewed.split("/")
-      const year = Number(date[1]);
-      
-      if (!selectedFilter) return true;
-
-      if (selectedFilter === "abv") {
-        return beer.abv <= 6;
-      }
-
-      if (selectedFilter === "ph") {
-        return beer.ph >= 4;
-      }
-
-      if (selectedFilter === "first_brewed") {
-        return year >= 2010
-      }
-
-      return false;
-    })
-    .filter((beer) => beer.name.toLowerCase().includes(searchTerm));*/
