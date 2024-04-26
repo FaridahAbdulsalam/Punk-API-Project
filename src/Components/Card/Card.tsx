e272d68e8cc28adaee788ca0581cb30902b609b4
Faridah Salami <f.salami@outlook.com>
2024-04-26 08:26:59 +0100

    Cards styled

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,51 +1,41 @@
 import Button from "../Button/Button";
-import "./Card.scss"
+import "./Card.scss";
 import { useState } from "react";
-
+import Cross from "../../assets/images/x-square (1).png";
 
 type CardProps = {
-    image: string;
-    name: any;
-    tagline: string;
-    ingredients: any;
-}
+  image: string;
+  name: any;
+  tagline: string;
+  ingredients: any;
+};
 
-const Card = ({image, name, tagline, ingredients} : CardProps) => {
+const Card = ({ image, name, tagline, ingredients }: CardProps) => {
   const [showInfo, setShowInfo] = useState<boolean>(false);
 
-
   const hancleClick = () => {
-    setShowInfo(!showInfo)
-  }
-
-  const cardInFront = (
-    <div onClick={hancleClick}>
-      <Button title="Find Out More"/>
-    </div>
-  );
-
-  const infoAtBack = (
-    <>
-    {ingredients}
-    <button onClick={hancleClick}>close</button>
-    </>
-  )
-
-  let infoClassName = "card__info "
-  if (showInfo) infoClassName += "card__info--info"
+    setShowInfo(!showInfo);
+  };
 
   return (
-    <div className='card'>
-      <img className='card__image' src={image} alt="image of beer"/>
-      <div className='card__content'>
-        <h1 className='card_name'>{name}</h1>
-        <p className='card__tag'>{tagline}</p>
-        <div className={infoClassName}>
-        {showInfo ? infoAtBack : cardInFront}
-        </div>
+    <div className="card">
+      <img className="card__image" src={image} alt="image of beer" />
+      <div className="card__content">
+        <h1 className="card_name">{name}</h1>
+        <p className="card__tag">{tagline}</p>
+        {!showInfo && (<div className="card__button">
+          <button onClick={hancleClick} className="card__info-open">Check out Ingredients</button>
+        </div> )}
+
+        {showInfo && (
+          <div className="card__info">
+            <img src={Cross} className="card__info-close" onClick={hancleClick}/>
+            <p>{ingredients}</p>
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
