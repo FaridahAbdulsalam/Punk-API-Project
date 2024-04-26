83ac9fbab338b312f5b0e897fcc3fcdde91bfe79
Faridah Salami <f.salami@outlook.com>
2024-04-26 03:29:35 +0100

    Navigation Bar styled

--- src/Components/NavigationBar/NavigationBar.tsx
+++ src/Components/NavigationBar/NavigationBar.tsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
 import "./NavigationBar.scss";
+import Home from "../../assets/images/house-door-fill .png"
+import Cards from "../../assets/images/front.png"
 
 const NavigationBar = () => {
   return (
     <nav className="nav-bar">
       <Link to="/">
-        <button className="nav-bar__home">Home</button>
+        <img src={Home} className="nav-bar__home"/>
       </Link>
       <h1 className="nav-bar__heading">BEER-necessities</h1>
       <Link to="/cards">
-        <button className="nav-bar__cards">All Cards</button>
+      <img src={Cards} className="nav-bar__cards"/>
       </Link>
     </nav>
   );
